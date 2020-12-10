bb88a5e6aeed92607221fe336e6d99383331dd2a
HorvathGergo <hgergo19@gmail.com>
2020-12-10 21:15:53 +0100

    todo was implemented -->needs refactoring

--- src/todo/Todo.java
+++ src/todo/Todo.java
@@ -1,24 +1,18 @@
 package todo;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Todo {
-    static final Path path = Paths.get("C:\\Users\\gerik\\Desktop\\greenfox\\hgergo19__todo_app\\src\\todo\\files\\tasks.txt");
-
-    static List<String> listOfTasks = new ArrayList<>();
-    static List<Task> tasks = new ArrayList<>();
-    static Map<String, String> arguments = new LinkedHashMap(4, .75F);
 
 
-    public static void checkArgument(String[] args) {
-        readFile();
+    Map<String, String> arguments = new LinkedHashMap(4, .75F);
+    TaskParser parser = new TaskParser();
+    String checkBox = "";
+
+
+    public void checkArgument(String[] args) {
         fillArguments();
         if (args.length == 0) {
             printInstructions();
@@ -26,41 +20,64 @@ public class Todo {
             throw new UnsupportedOperationException();
         } else {
             if (args[0].equals("-l")) {
-                String tmp = listOfTasks.size() == 0 ? "No todos for today! :)" : printTasks();
-                System.out.println(tmp);
+                listTasks();
             } else if (args[0].equals("-a")) {
-                listOfTasks.add(new Task(args[1]).toString());
-                writeFile();
+                if (args.length == 1) {
+                    System.out.println("Unable to add: no task provided");
+                } else {
+                    addTask(args[1]);
+                }//TODO : Exception handling
+            } else if (args[0].equals("-r")) {
+                removeTask(args[1]);
+            } else if (args[0].equals("-c")) {
+                completeTask(args[1]);
             }
         }
     }
 
-    private static void writeFile() {
-        try {
-            Files.write(path, listOfTasks);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void completeTask(String arg) {
+        parser.getTasks().get(Integer.parseInt(arg) - 1).setCompleted();
+        parser.refillContentLines();
+        successfulOperation();
     }
 
-
-    private static String printTasks() {
-        StringBuilder tmp = new StringBuilder();
-        for (int i = 0; i < listOfTasks.size(); i++) {
-            tmp.append(i).append(" - ").append(listOfTasks.get(i)).append("\n");
-        }
-        return tmp.toString();
+    private void addTask(String arg) {
+        parser.getTasks().add(new Task(arg, false));
+        parser.refillContentLines();
+        successfulOperation();
     }
 
-    private static void readFile() {
-        try {
-            listOfTasks = Files.readAllLines(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void removeTask(String arg) {
+        parser.getTasks().remove(Integer.parseInt(arg) - 1);
+        parser.refillContentLines();
+        successfulOperation();
     }
 
-    private static void printInstructions() {
+    private void listTasks() {
+        List<Task> tasks = parser.getTasks();
+        if (tasks.size() == 0) {
+            System.out.println("No todos for today! :)");
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).isCompleted()) {
+                checkBox = "[X]";
+            } else {
+                checkBox = "[ ]";
+            }
+            stringBuilder.append(i + 1).append(" - ").append(checkBox).append(" ").append(tasks.get(i).getDescription()).append("\n");
+
+        }
+        System.out.println("\n" + stringBuilder);
+    }
+
+    private void successfulOperation() {
+        System.out.println("Operation finished successfully!");
+    }
+
+    private void printInstructions() {
+        System.out.println();
+        System.out.println();
         System.out.println("Command Line Todo application " +
                 "\n============================= \n" +
                 "\nCommand line arguments:");
@@ -69,11 +86,12 @@ public class Todo {
         }
     }
 
-    private static void fillArguments() {
+    private void fillArguments() {
         arguments.put("-l", "Lists all the tasks");
         arguments.put("-a", "Adds a new task");
         arguments.put("-r", "Removes a task");
         arguments.put("-c", "Completes a task");
     }
+
 
 }

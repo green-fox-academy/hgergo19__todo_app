e7384f54f4db3dbffc1a259db2d051cea38d586b
HorvathGergo <hgergo19@gmail.com>
2020-12-11 15:34:17 +0100

    print undone tasks was added

--- src/todo/Printer.java
+++ src/todo/Printer.java
@@ -4,29 +4,45 @@ import java.util.List;
 import java.util.Map;
 
 public abstract class Printer {
+    static private String checkBox;
 
     public static void printInfo(TaskParser parser) {
-        String checkBox;
-        {
-            List<Task> tasks = parser.getTasks();
-            if (tasks.size() == 0) {
-                System.out.println("No todos for today! :)");
-            }
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < tasks.size(); i++) {
-                if (tasks.get(i).isCompleted()) {
-                    checkBox = "[X]";
-                } else {
-                    checkBox = "[ ]";
-                }
-                stringBuilder.append(i + 1).append(" - ").append(checkBox).append(" ").append(tasks.get(i).getDescription()).append("\n");
-            }
-            System.out.println("\n" + stringBuilder);
+
+        List<Task> tasks = parser.getTasks();
+        if (tasks.size() == 0) {
+            System.out.println("No todos for today! :)");
         }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).isCompleted()) {
+                checkBox = "[X]";
+            } else {
+                checkBox = "[ ]";
+            }
+            stringBuilder.append(i + 1).append(" - ").append(checkBox).append(" ").append(tasks.get(i).getDescription()).append("\n");
+        }
+        System.out.println("\n" + stringBuilder);
     }
 
-    public static void successfulOperation() {
-        System.out.println("Operation finished successfully!");
+    public static void printUndone(TaskParser parser) {
+        System.out.println("Undone tasks: ");
+        List<Task> tasks = parser.getTasks();
+        if (tasks.size() == 0) {
+            System.out.println("No todos for today! :)");
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < tasks.size(); i++) {
+            if (!tasks.get(i).isCompleted()) {
+                checkBox = "[ ]";
+                stringBuilder.append(i + 1).append(" - ").append(checkBox).append(" ").append(tasks.get(i).getDescription()).append("\n");
+            }
+
+        }
+        System.out.println("\n" + stringBuilder);
+    }
+
+    public static void successfulOperation(String operation) {
+        System.out.println(operation + " operation finished successfully!");
     }
 
     public static void printInstructions(Map<String, String> arguments) {
